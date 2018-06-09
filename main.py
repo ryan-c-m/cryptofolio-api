f9a21b351a9f6f6520a955e168c2a2446ba48274
Ryan Morris <ryan.c.morris1@gmail.com>
2018-06-10 03:02:31 +1000

    get and save json to datastore

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from flask import Flask
-
+from flask import request, Flask, jsonify
+from google.appengine.ext import ndb
 
 app = Flask(__name__)
 
+@app.route('/get', methods=['GET'])
+def get():
+    key = request.args.get('id')
+    account_key = ndb.Key(Profile, key)
+    profile = account_key.get()
+    return jsonify(profile.to_dict())
 
-@app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/save', methods=['POST'])
+def save():
+    profile = Profile(id=request.json['id'])
+    profile.data = request.json['data']
+    profile.put()
+    return 'ok', 204
 
 
 if __name__ == '__main__':
     app.run()
+
+
+class Profile(ndb.Model):
+    data = ndb.StringProperty()
